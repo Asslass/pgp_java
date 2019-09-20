91cb068a3abda84b9061aa3b125337da3f63a742
Asslass <lcx_bupt@163.com>
2019-09-20 16:03:26 +0800

    create a new java exercise project

---
+++ src/com/hit/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.hit.demo;
+
+/**
+ * creater:ASSLASS
+ * create time:2019/9/19
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}

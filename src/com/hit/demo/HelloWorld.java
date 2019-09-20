ed1732aaa451b92bb2e68d36098a458ff80366aa
Asslass <lcx_bupt@163.com>
2019-09-20 16:36:16 +0800

    test branch div

--- src/com/hit/demo/HelloWorld.java
+++ src/com/hit/demo/HelloWorld.java
@@ -7,5 +7,6 @@ package com.hit.demo;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("And i have made a new branch!");
     }
 }

96cb0ff43275900fc7a30967239e409ba582f965
Asslass <lcx_bupt@163.com>
2019-09-21 13:36:12 +0800

    if exercise

---
+++ src/com/hit/demo/Gerif.java
@@ -0,0 +1,19 @@
+package com.hit.demo;
+
+/**
+ * @author Asslass
+ * create time:2019/9/21
+ * about:if 练习
+ */
+public class Gerif {
+    public static void main(String[] args){
+        int x = 45;
+        int y = 12;
+        if (x > y){
+            System.out.println("变量x大于y");
+        }
+        if (x < y){
+            System.out.println("变量x小于y");
+        }
+    }
+}
